0dd7ee0cb00f8a45a38ba53c89192ffdbf7256ad
PradSonkeng <prad.sonkeng@facsciences-uy1.cm>
2026-03-22 16:20:39 +0100

    feat: création de la base de données

---
+++ migrations/20260321183738-create-article.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Articles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      titre: {
+      	allowNull: false,
+        type: Sequelize.STRING
+      },
+      auteur: {
+      	allowNull: false,
+        type: Sequelize.STRING
+      },
+      categorie: {
+      	allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Articles');
+  }
+};
